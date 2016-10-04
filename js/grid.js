48418c0331dd2e82103e3b8ef95b881f7f0f4c8a
quietsamurai98 <quietsamurai98@gmail.com>
2016-10-04 17:28:30 -0500

    Update grid.js

--- js/grid.js
+++ js/grid.js
@@ -148,7 +148,7 @@ Grid.prototype.addStartTiles = function () {
 // Adds a tile in a random position
 Grid.prototype.addRandomTile = function () {
   if (this.cellsAvailable()) {
-    var value = Math.random() < 0.9 ? 2 : 4;
+    var value = 2;
     //var value = Math.random() < 0.9 ? 256 : 512;
     var tile = new Tile(this.randomAvailableCell(), value);
 
